33bb885d4696ff085efce2bbf44b4f4ecd1f80a7
koopdev <koop@koop.codes>
2018-08-02 12:08:43 -0700

    Simple HTTP Server

---
+++ simple-node-http-server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Writing file.....', fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
+  if (err) throw err;
+  console.log('success')
+}));
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port}`);
+})
